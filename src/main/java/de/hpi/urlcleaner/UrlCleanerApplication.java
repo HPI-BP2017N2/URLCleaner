3e53b295899c71b2a6f478f3312f2afcb961dbec
deeps96 <leonardo.huebscher.business@gmail.com>
2018-04-04 11:44:01 +0200

    Setup the basic spring environment. #1

---
+++ src/main/java/de/hpi/urlcleaner/UrlCleanerApplication.java
@@ -0,0 +1,12 @@
+package de.hpi.urlcleaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlCleanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlCleanerApplication.class, args);
+	}
+}
