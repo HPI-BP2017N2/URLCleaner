733a612dd8f40d0f3a0099be2344f79261aec009
deeps96 <leonardo.huebscher.business@gmail.com>
2018-04-09 17:51:12 +0200

    Add expire time to blacklist entry. #7

--- src/main/java/de/hpi/urlcleaner/persistence/BlacklistEntry.java
+++ src/main/java/de/hpi/urlcleaner/persistence/BlacklistEntry.java
@@ -2,12 +2,22 @@ package de.hpi.urlcleaner.persistence;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.Date;
 
 @Document(collection = "blacklist")
 @Getter
 @RequiredArgsConstructor
 public class BlacklistEntry {
+
+    private static final int EXPIRES_AFTER_SECONDS = 60 * 60 * 24 * 30;
+
+    @Field
+    @Indexed(name = "createdTimestamp", expireAfterSeconds = EXPIRES_AFTER_SECONDS)
+    private final Date created;
 
     private final long shopID;
 
